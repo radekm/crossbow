2ba6e537f7ebab8228e3cb743caa9f42296977df
Radek Micek <radek.micek@gmail.com>
2013-01-07 01:08:38 +0100

    Add module Symb

---
+++ src/symb.ml
@@ -0,0 +1,104 @@
+(* Copyright (c) 2013 Radek Micek *)
+
+type id = int
+
+type name = string
+
+type arity = int
+
+type symbol = {
+  s_id : id;
+  s_name : name;
+  s_arity : arity;
+  s_commutative : bool;
+}
+
+type db = {
+  by_id : symbol BatDynArray.t;
+  by_name : (string * int, id) Hashtbl.t;
+}
+
+let max_arity = 255
+
+let create_db () =
+  let by_id = BatDynArray.make 20 in
+  let by_name = Hashtbl.create 20 in
+
+  BatDynArray.add by_id {
+    s_id = 0;
+    s_name = "=";
+    s_arity = 2;
+    s_commutative = true;
+  };
+  Hashtbl.add by_name ("=", 2) 0;
+
+  BatDynArray.add by_id {
+    s_id = 1;
+    s_name = "~";
+    s_arity = 1;
+    s_commutative = false;
+  };
+  Hashtbl.add by_name ("~", 1) 1;
+
+  { by_id; by_name }
+
+let add_symb db name arity =
+  if String.length name <= 0 then invalid_arg "name";
+  if arity < 0 || arity > max_arity then invalid_arg "arity";
+  if Hashtbl.mem db.by_name (name, arity) then failwith "duplicate symbol";
+  let id = BatDynArray.length db.by_id in
+  BatDynArray.add db.by_id {
+    s_id = id;
+    s_name = name;
+    s_arity = arity;
+    s_commutative = false;
+  };
+  Hashtbl.add db.by_name (name, arity) id;
+  id
+
+let mem db name arity = Hashtbl.mem db.by_name (name, arity)
+
+let find db name arity = Hashtbl.find db.by_name (name, arity)
+
+let add_anon_symb db arity =
+  if arity < 0 || arity > max_arity then invalid_arg "arity";
+  let id = BatDynArray.length db.by_id in
+  BatDynArray.add db.by_id {
+    s_id = id;
+    s_name = "";
+    s_arity = arity;
+    s_commutative = false;
+  };
+  id
+
+let iter f db =
+  for i = 0 to BatDynArray.length db.by_id - 1 do
+    f i
+  done
+
+let sym_eq = 0
+
+let sym_not = 1
+
+(* ************************************************************************* *)
+(* Properties of symbols *)
+
+let get db sym =
+  if BatDynArray.length db.by_id <= sym then raise Not_found;
+  BatDynArray.get db.by_id sym
+
+let name db sym = (get db sym).s_name
+
+let arity db sym = (get db sym).s_arity
+
+let commutative db sym = (get db sym).s_commutative
+
+let set_commutative db sym comm =
+  if sym = sym_eq || sym = sym_not then
+    failwith "predefined symbol";
+  let symb = get db sym in
+  if symb.s_arity <> 2 then
+    failwith "non-binary symbol";
+  BatDynArray.set db.by_id sym { symb with s_commutative = comm }
+
+let anon db sym = name db sym = ""
---
+++ src/symb.mli
@@ -0,0 +1,88 @@
+(* Copyright (c) 2013 Radek Micek *)
+
+(** Symbol databases.
+
+   A symbol is a pair [name/arity]. A symbol database is a collection of symbols
+   and their properties. Every symbol in a database has a unique [id] which
+   was assigned when the symbol was added to the database.
+*)
+
+(** Symbol database. *)
+type db
+
+(** An identifier of a symbol in a database.
+   Identifiers are not portable across databases.
+*)
+type id
+
+(** Symbol name. *)
+type name = string
+
+(** Symbol arity. *)
+type arity = int
+
+val max_arity : int
+
+(** Creates a new symbol database containing only the predefined symbols. *)
+val create_db : unit -> db
+
+(** Adds the symbol into the database.
+
+   Raises [Failure] if the symbol already exists in the database.
+   Raises [Invalid_argument] if the name is empty or the arity is out of the range.
+*)
+val add_symb : db -> name -> arity -> id
+
+(** Tests if the symbol is in the database. *)
+val mem : db -> name -> arity -> bool
+
+(** Finds the symbol by the name and the arity and returns its identifier.
+
+   Raises [Not_found] if the symbol is not in the database.
+*)
+val find : db -> name -> arity -> id
+
+(** Adds a new anonymous symbol into the database.
+   Anonymous symbols are ignored by {!mem} and {!find}.
+
+   Raises [Invalid_argument] if the arity is out of the range.
+*)
+val add_anon_symb : db -> arity -> id
+
+(** [iter f db] applies [f] to each symbol in the database [db]. *)
+val iter : (id -> unit) -> db -> unit
+
+(** {6 Properties of symbols}
+
+   These functions raise [Not_found] if the symbol is not in the database.
+*)
+
+(** Returns the name. Empty string for anonymous symbols. *)
+val name : db -> id -> name
+
+(** Returns the arity. *)
+val arity : db -> id -> arity
+
+(** Returns whether the symbol is commutative. *)
+val commutative : db -> id -> bool
+
+(** Sets commutativity of the binary symbol.
+   Must not be used for predefined symbols.
+
+   Raises [Failure] if the symbol is predefined or non-binary.
+*)
+val set_commutative : db -> id -> bool -> unit
+
+(** Returns whether the symbol is anonymous. *)
+val anon : db -> id -> bool
+
+(** {6 Predefined symbols}
+
+   Identifiers of these symbols are same across all databases.
+*)
+
+(** The id of the equality symbol [=/2]. *)
+val sym_eq : id
+
+(** The id of the negation symbol [~/1]. *)
+val sym_not : id
