5fb983b0d11b9429e0c8de5a5ec2c2f0d78094bd
Radek Micek <radek.micek@gmail.com>
2013-06-23 18:59:28 +0200

    Add tests for Symb

--- test/test_symb.ml
+++ test/test_symb.ml
@@ -1,6 +1,56 @@
 
 open OUnit
 
+let test_ids_differ () =
+  let Symb.Wr db = Symb.create_db () in
+  let s1 = Symb.add_func db 2 in
+  let s2 = Symb.add_func db 2 in
+  let s3 = Symb.add_pred db 2 in
+  let s4 = Symb.add_func db 1 in
+  let s5 = Symb.add_pred db 1 in
+  let s6 = Symb.add_pred db 1 in
+  let s7 = Symb.add_func db 0 in
+  let s8 = Symb.add_pred db 0 in
+  let s9 = Symb.add_func db 0 in
+  let all = [Symb.sym_eq; s1; s2; s3; s4; s5; s6; s7; s8; s9] in
+  assert_equal (List.length all) (List.length (BatList.unique all))
+
+let test_ids_order () =
+  let Symb.Wr db = Symb.create_db () in
+  let s1 = Symb.add_func db 2 in
+  let s2 = Symb.add_pred db 1 in
+  let s3 = Symb.add_pred db 2 in
+  let s4 = Symb.add_func db 1 in
+  let s5 = Symb.add_func db 3 in
+  assert_bool "" (Symb.sym_eq < s1);
+  assert_bool "" (s1 < s2);
+  assert_bool "" (s2 < s3);
+  assert_bool "" (s3 < s4);
+  assert_bool "" (s4 < s5)
+
+let test_arity_kind () =
+  let Symb.Wr db = Symb.create_db () in
+  let s1 = Symb.add_pred db 3 in
+  let s2 = Symb.add_func db 0 in
+  let s3 = Symb.add_func db Symb.max_arity in
+  let s4 = Symb.add_pred db Symb.max_arity in
+  let s5 = Symb.add_func db 2 in
+  let s6 = Symb.add_pred db 0 in
+  (* Arity. *)
+  assert_equal 3 (Symb.arity s1);
+  assert_equal 0 (Symb.arity s2);
+  assert_equal Symb.max_arity (Symb.arity s3);
+  assert_equal Symb.max_arity (Symb.arity s4);
+  assert_equal 2 (Symb.arity s5);
+  assert_equal 0 (Symb.arity s6);
+  (* Kind. *)
+  assert_equal Symb.Pred (Symb.kind s1);
+  assert_equal Symb.Func (Symb.kind s2);
+  assert_equal Symb.Func (Symb.kind s3);
+  assert_equal Symb.Pred (Symb.kind s4);
+  assert_equal Symb.Func (Symb.kind s5);
+  assert_equal Symb.Pred (Symb.kind s6)
+
 let test_iter () =
   let Symb.Wr db = Symb.create_db () in
   let s1 = Symb.add_func db 2 in
@@ -60,6 +110,9 @@ let test_set_auxiliary_rejects_predefined_symbs () =
 let suite =
   "Symb suite" >:::
     [
+      "ids differ" >:: test_ids_differ;
+      "order of ids" >:: test_ids_order;
+      "arity, kind" >:: test_arity_kind;
       "iter" >:: test_iter;
       "set_commutative" >:: test_set_commutative;
       "set_commutative rejects predefined symbols" >::
