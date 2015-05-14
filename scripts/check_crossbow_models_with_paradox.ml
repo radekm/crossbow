6bb4e9b1930ab7b3cc504af8cc21f2194df5dc9c
Radek Micek <radek.micek@gmail.com>
2015-05-14 20:05:16 +0200

    Scripts: Check that models contain exactly symbols from problem

--- scripts/check_crossbow_models_with_paradox.ml
+++ scripts/check_crossbow_models_with_paradox.ml
@@ -4,6 +4,112 @@ module Res = Report.Result
 module RS = Run_shared
 module Ast = Tptp_ast
 
+module Symbols = struct
+
+  type arity = int
+
+  type symb =
+    | Atomic_word of Ast.atomic_word * arity
+    | Number of Q.t
+    | String of Ast.tptp_string
+
+  (** Calls [pred] resp. [func] for each occurence of a predicate
+     resp. a function symbol in the input formula.
+
+     Predicates [$false/0], [$true/0] and [=/2] are ignored.
+     Occurences of symbols in annotations are ignored.
+  *)
+  let iter_symbols pred func input =
+    let rec proc_term = function
+      | Ast.Var _ -> ()
+      | Ast.Func (symb, args) ->
+          func (Atomic_word (symb, List.length args));
+          List.iter proc_term args
+      | Ast.Number n -> func (Number n)
+      | Ast.String s -> func (String s) in
+    let proc_atom = function
+      | Ast.Equals (a, b) ->
+          proc_term a;
+          proc_term b
+      | Ast.Pred (Ast.Defined_word s, [])
+        when List.mem (s :> string) ["$false"; "$true"] -> ()
+      | Ast.Pred (symb, args) ->
+          pred (Atomic_word (symb, List.length args));
+          List.iter proc_term args in
+    let rec proc_formula = function
+      | Ast.Binop (_, a, b) ->
+          proc_formula a;
+          proc_formula b
+      | Ast.Not a | Ast.Quant (_, _, a) -> proc_formula a
+      | Ast.Atom a -> proc_atom a in
+    let proc_lit (Ast.Lit (_, a)) = proc_atom a in
+
+    match input with
+      | Ast.Fof_anno { Ast.af_formula } ->
+          begin match af_formula with
+            | Ast.Sequent (xs, ys) ->
+                List.iter proc_formula xs;
+                List.iter proc_formula ys
+            | Ast.Formula x -> proc_formula x
+          end
+      | Ast.Cnf_anno { Ast.af_formula = Ast.Clause lits } ->
+          List.iter proc_lit lits
+      | Ast.Include _ -> failwith "iter_symbols: unexpected include"
+      | Ast.Comment _ -> ()
+
+  type t = {
+    mutable preds : symb BatSet.t;
+    mutable funcs : symb BatSet.t;
+  }
+
+  let create () = {
+    preds = BatSet.empty;
+    funcs = BatSet.empty;
+  }
+
+  let add symbs input =
+    let add_pred s = symbs.preds <- BatSet.add s symbs.preds in
+    let add_func s = symbs.funcs <- BatSet.add s symbs.funcs in
+    iter_symbols add_pred add_func input
+
+  let print_set desc symbs =
+    let show_symb s =
+      match s with
+        | Atomic_word (aw, arity) ->
+            Printf.sprintf "%s/%d" (Tptp_printer.show_atomic_word aw) arity
+        | Number n -> Q.to_string n
+        | String s -> Tptp_printer.show_tptp_string s in
+    let strs = BatSet.to_list symbs |> BatList.map show_symb in
+    if strs <> [] then
+      Printf.printf "%s: %s\n" desc (String.concat ", " strs)
+
+  (** Returns true if the model contains all symbols from the problem.
+
+     Prints warning if the model doesn't contain all symbols
+     from the problem.
+     Prints warning if the model contains symbols except integers
+     which aren't in the problem (integers are used as domain elements).
+  *)
+  let check ~symbs_in_model:m ~symbs_in_problem:p =
+    let is_integer s =
+      match s with
+        | Number n -> n = Q.zero || Q.den n = Z.one
+        | Atomic_word _
+        | String _ -> false in
+    let missing_preds = BatSet.diff p.preds m.preds in
+    let missing_funcs = BatSet.diff p.funcs m.funcs in
+    let additional_preds = BatSet.diff m.preds p.preds in
+    let additional_funcs =
+      BatSet.diff m.funcs p.funcs
+      |> BatSet.filter (fun s -> not (is_integer s)) in
+    print_set "Missing preds" missing_preds;
+    print_set "Missing funcs" missing_funcs;
+    print_set "Additional preds" additional_preds;
+    print_set "Additional funcs" additional_funcs;
+    BatSet.is_empty missing_preds && BatSet.is_empty missing_funcs
+
+end
+
 let iter_models
     (file_with_models : string)
     (f : int -> Ast.tptp_input list -> unit)
@@ -46,6 +152,9 @@ let check_model_with_paradox
     max_mem
     dsize
     model_formulas =
+
+  let symbs_in_model = Symbols.create () in
+  let symbs_in_problem = Symbols.create () in
   let paradox_in = BatFile.with_temporary_out
     (fun out file ->
       (* Paradox doesn't treat integer constants as distinct,
@@ -67,12 +176,13 @@ let check_model_with_paradox
         done
       done;
 
-      (* Write model formulas. *)
+      (* Write model formulas and record symbols in model. *)
       BatList.iter
         (function
           | Ast.Fof_anno af ->
               let tptp_input =
                 Ast.Fof_anno { af with Ast.af_role = Ast.R_axiom } in
+              Symbols.add symbs_in_model tptp_input;
               BatIO.nwrite out (Tptp.to_string tptp_input)
           | Ast.Comment _
           | Ast.Cnf_anno _
@@ -80,14 +190,22 @@ let check_model_with_paradox
               failwith "check_model_with_paradox: unexpected tptp_input")
         model_formulas;
 
-      (* Write problem formulas. *)
+      (* Write problem formulas and record symbols in problem. *)
       Tptp.File.iter
         ~base_dir
         (fun tptp_input ->
+          Symbols.add symbs_in_problem tptp_input;
           BatIO.nwrite out (Tptp.to_string tptp_input))
         problem_file;
 
       file) in
+
+  (* Ensure that the model contains all symbols from the problem. *)
+  if not (Symbols.check ~symbs_in_model ~symbs_in_problem) then begin
+    flush stdout;
+    Sys.remove paradox_in;
+    failwith "Invalid model";
+  end;
 
   (* Execute Paradox. *)
   let paradox_out = BatFile.with_temporary_out (fun _ file -> file) in
